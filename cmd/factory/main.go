4ee4ea1aa6c3a6807e7aa512191a36a6b58e2f8e
imran4u <er.imran4u@gmail.com>
2024-11-27 17:52:20 +0530

    Factory pattern

---
+++ cmd/factory/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	speak() string
+}
+
+type Cat struct{}
+
+func (c *Cat) speak() string {
+	return "Meow"
+}
+
+type Dog struct{}
+
+func (d *Dog) speak() string {
+	return "Bark/ Bhow"
+}
+
+func AnimalFactory(name string) Animal {
+	switch name {
+	case "cat":
+		return &Cat{}
+	case "dog":
+		return &Dog{}
+	default:
+		return nil
+	}
+}
+
+func main() {
+	dog := AnimalFactory("dog")
+	cat := AnimalFactory("cat")
+	fmt.Println(dog.speak())
+	fmt.Println(cat.speak())
+
+}
